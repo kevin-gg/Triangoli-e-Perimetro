d3d8e4a4875ae8c6d2291a582ad9e6b4f3be8ae7
kevin trentini <trentinikevin6@gmail.com>
2020-11-25 22:21:27 +0100

    Add project files.

---
+++ Funzioni/Funzioni.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Funzioni
+{
+    public class Funzioni
+    {
+        public static double InserisciLato()
+        {
+            Console.WriteLine("Inserisci il lato");
+            double n = Convert.ToDouble(Console.ReadLine());
+            return n;
+        }
+        public static void Triangolo(double n1, double n2, double n3)
+        {
+
+            if (n1 == n2 && n1 == n3)
+            {
+                Console.WriteLine("Il triangolo è Equilatero");
+            }
+            else if (n1 == n2 || n1 == n3 || n2 == n3)
+            {
+                Console.WriteLine("Il triangolo è Isoscele");
+            }
+            else
+            {
+                Console.WriteLine("Il triangolo è Scaleno");
+            }
+
+
+        }
+        public static void Perimetro(double n1, double n2, double n3)
+        {
+
+
+             Console.WriteLine($"Perimetro = {n1 + n2 + n3}");
+           
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
---
+++ prova/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Funzioni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Funzioni.InserisciLato();
+            double b = Funzioni.InserisciLato();
+            double c = Funzioni.InserisciLato();
+            Funzioni.Triangolo(a, b, c);
+            Funzioni.Perimetro(a, b, c);
+        }
+    }
+}
